7e10c43c43418e9897667c9fac2339e22b2f939b
oleksandr-vt <96267599+oleksandr-vt@users.noreply.github.com>
2022-07-12 18:25:07 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,123 @@
+// Custom Scripts
+// Custom scripts
+const btn1 = document.querySelector('.button1');
+const btn2 = document.querySelector('.button2');
+const btn3 = document.querySelector('.button3');
+
+const trust = document.querySelector('.trust');
+const exodus = document.querySelector('.exodus');
+const ambire = document.querySelector('.ambire');
+const blueDot = document.querySelector('.blue_dot');
+const cryptoCom = document.querySelector('.crypto_com');
+const bitGo = document.querySelector('.bitgo');
+const binance = document.querySelector('.binance');
+const ledgerWallet = document.querySelector('.ledger_wallet');
+const kraken = document.querySelector('.kraken');
+const blockfi = document.querySelector('.blockfi');
+const mycelium = document.querySelector('.mycelium');
+const trezor = document.querySelector('.trezor');
+const coinPayments = document.querySelector('.coin_payments');
+const metamask = document.querySelector('.metamask');
+
+const bigCommerce = document.querySelector('.big_commerce');
+const wooCommerce = document.querySelector('.woo_commerce');
+const magento = document.querySelector('.magento');
+const shopify = document.querySelector('.shopify');
+
+const bitcoin = document.querySelector('.bitcoin');
+const green = document.querySelector('.green');
+const northAmerican = document.querySelector('.north_american');
+const z = document.querySelector('.z');
+
+function btnClick1() {
+    btn1.classList.add('active');
+    btn2.classList.remove('active');
+    btn3.classList.remove('active');
+
+    trust.classList.add('active');
+    exodus.classList.add('active');
+    ambire.classList.add('active');
+    blueDot.classList.add('active');
+    cryptoCom.classList.add('active');
+    bitGo.classList.add('active');
+    binance.classList.add('active');
+    ledgerWallet.classList.add('active');
+    kraken.classList.add('active');
+    blockfi.classList.add('active');
+    mycelium.classList.add('active');
+    trezor.classList.add('active');
+    coinPayments.classList.add('active');
+    metamask.classList.add('active');
+
+    bigCommerce.classList.remove('active');
+    wooCommerce.classList.remove('active');
+    magento.classList.remove('active');
+    shopify.classList.remove('active');
+
+    bitcoin.classList.remove('active');
+    green.classList.remove('active');
+    northAmerican.classList.remove('active');
+    z.classList.remove('active');
+}
+
+function btnClick2() {
+    btn1.classList.remove('active');
+    btn2.classList.add('active');
+    btn3.classList.remove('active');
+
+    trust.classList.remove('active');
+    exodus.classList.remove('active');
+    ambire.classList.remove('active');
+    blueDot.classList.remove('active');
+    cryptoCom.classList.remove('active');
+    bitGo.classList.remove('active');
+    binance.classList.remove('active');
+    ledgerWallet.classList.remove('active');
+    kraken.classList.remove('active');
+    blockfi.classList.remove('active');
+    mycelium.classList.remove('active');
+    trezor.classList.remove('active');
+    coinPayments.classList.remove('active');
+    metamask.classList.remove('active');
+
+    bigCommerce.classList.add('active');
+    wooCommerce.classList.add('active');
+    magento.classList.add('active');
+    shopify.classList.add('active');
+
+    bitcoin.classList.remove('active');
+    green.classList.remove('active');
+    northAmerican.classList.remove('active');
+    z.classList.remove('active');
+}
+
+function btnClick3() {
+    btn1.classList.remove('active');
+    btn2.classList.remove('active');
+    btn3.classList.add('active');
+
+    trust.classList.remove('active');
+    exodus.classList.remove('active');
+    ambire.classList.remove('active');
+    blueDot.classList.remove('active');
+    cryptoCom.classList.remove('active');
+    bitGo.classList.remove('active');
+    binance.classList.remove('active');
+    ledgerWallet.classList.remove('active');
+    kraken.classList.remove('active');
+    blockfi.classList.remove('active');
+    mycelium.classList.remove('active');
+    trezor.classList.remove('active');
+    coinPayments.classList.remove('active');
+    metamask.classList.remove('active');
+
+    bigCommerce.classList.remove('active');
+    wooCommerce.classList.remove('active');
+    magento.classList.remove('active');
+    shopify.classList.remove('active');
+
+    bitcoin.classList.add('active');
+    green.classList.add('active');
+    northAmerican.classList.add('active');
+    z.classList.add('active');
+}
